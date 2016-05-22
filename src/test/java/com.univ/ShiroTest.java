bed63bd38151ca49df42184889b907b246b9d60f
jssgsy <fcsnwu@163.com>
2016-05-22 19:05:30 +0800

    初建shiro web项目，导入shiro-core jar包

---
+++ src/test/java/com.univ/ShiroTest.java
@@ -0,0 +1,20 @@
+package com.univ;
+
+import org.junit.Test;
+
+/**
+ * created by Univ
+ * 16/5/22 18:56
+ */
+public class ShiroTest {
+
+    public static void main(String[] args) {
+        System.out.println("[univ]:------ " );
+    }
+
+    @Test
+    public void test(){
+        System.out.println("[univ]:------ " );
+    }
+
+}
